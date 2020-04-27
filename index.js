91d49edaaef070da968be08d15e8791ce4ced8f4
Victor <victor.dedios@outlook.com>
2020-04-27 22:46:57 +0200

    add routing functionalities

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ const map = L.map('map', {
    zoomControl: false
 });
 
-async function onMapClick() {
-   //We will write code in here later...
-}
+let startCoordinates = '';
+
+document.getElementById('clear').onclick = clearMap;
+document.getElementById('change-start').onclick = addStartingMarker;
+addStartingMarker();
 
 async function geocode(query) {
    const url = `https://geocoder.ls.hereapi.com/6.2/geocode.json
@@ -28,3 +30,35 @@ async function geocode(query) {
    return await data.Response.View[0].Result[0].Location.NavigationPosition[0];
 }
 
+async function route(start, end) {
+   const mode = document.querySelector('input[name="routing-mode"]:checked').value;
+   const url = `https://route.ls.hereapi.com/routing/7.2/calculateroute.json
+?apiKey=${here.apiKey}&waypoint0=geo!${start}&waypoint1=geo!${end}&mode=fastest;${mode};traffic:disabled&routeattributes=shape`
+   const response = await fetch(url);
+   const data = await response.json();
+   return await data.response.route[0];
+}
+
+function clearMap() {
+   map.eachLayer((layer) => {
+      if (!layer.hasOwnProperty('_updating_tangram') && !layer.options.hasOwnProperty('alt')) {
+         map.removeLayer(layer);
+      }
+   });
+}
+
+async function addStartingMarker() {
+   clearMap();
+   const startAddress = document.getElementById('start').value;
+   startCoordinates = await geocode(startAddress);
+   const startingCircle = L.marker([startCoordinates.Latitude, startCoordinates.Longitude], {alt: 'start'}).addTo(map);
+}
+
+async function onMapClick(selection) {
+   if (selection.feature) {
+      const endCoordinates = `${selection.leaflet_event.latlng.lat},${selection.leaflet_event.latlng.lng}`;
+      const routeData = await route(`${startCoordinates.Latitude},${startCoordinates.Longitude}`, endCoordinates);
+      const shape = routeData.shape.map(x => x.split(","));
+      const poly = L.polyline(shape).addTo(map).snakeIn();
+   }
+}
